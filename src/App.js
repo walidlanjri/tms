d0866a9fa938ec3c5d1f69b842c6da8ac8d67e6d
walidlanjri <waleedlanjri@gmail.com>
2022-05-30 21:32:19 +0100

    Add navBar for Home and split CSS files

--- src/App.js
+++ src/App.js
@@ -3,28 +3,37 @@ import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 import HomeFooter from "./HomeLogin/Footer";
 import HomeBody from "./HomeLogin/Home";
 import Login from "./HomeLogin/Login";
+import NavBar from "./HomePage/NavBar";
 
 function App() {
   return (
     <Router>
       <div className="app">
 
-        <NavLogin/>
+        
 
         <Switch>
           <Route exact path="/">
+            <NavLogin/>
             <HomeBody/>
+            <HomeFooter/>
           </Route>
           
           <Route path="/Login">
+            <NavLogin/>
             <Login/>
+            <HomeFooter/>
+          </Route>
+
+          <Route path="/Home">
+            <NavBar/>
           </Route>
 
         </Switch>
 
         
 
-        <HomeFooter/>
+        
         
       </div>
     </Router>
--- src/HomeLogin/NavBar.js
+++ src/HomeLogin/NavBar.js
@@ -1,13 +1,13 @@
 const NavBar = () => {
     return ( 
-        <div className="navLogin">
+        <div className="navBar">
             <a href="/">
             <img src="transport64.png" alt="logo" />
             </a>
             <h1> MyTMS</h1>
             <div className="loginButtons">
-            <a href="/Login"><button>Se connecter</button></a>
-            <button>S'inscrire</button>
+                <a href="/Login"><button>Se connecter</button></a>
+                <button>S'inscrire</button>
             </div>
       </div>
      );
